0ed6b534299f2194074d22354148ebe623fa316b
student <student@Achievers-iMac.local>
2017-04-07 14:44:10 +0530

    Initial Commit

---
+++ PulseApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PulseApp
+//
+//  Created by student on 4/7/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PulseApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PulseApp
+//
+//  Created by student on 4/7/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
